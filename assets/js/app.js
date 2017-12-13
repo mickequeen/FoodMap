0c3e1e0665b33a338b2789657df0ce5addaf5799
mickequeen <mickequeen@gmail.com>
2017-12-12 22:04:07 -0300

    loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     setTimeout(function() {
       $(".splash").fadeOut(1500);
-    },1000);
+    },2500);
     
+    
+    setTimeout(function() {
+        $(".content").fadeIn(1500);
+    },3500);
+	
 });
