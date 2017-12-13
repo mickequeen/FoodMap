da76f65e2927d0fc3a91df49f7594bfb93a44282
mickequeen <mickequeen@gmail.com>
2017-12-13 02:09:59 -0300

    filtro listo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
-    setTimeout(function() {
-      $(".splash").fadeOut(1500);
-    },2500);
-    
-    
-    setTimeout(function() {
-        $(".content").fadeIn(1500);
-    },3500);
-	
+	/*
+  *función de splash
+  */
+  setTimeout(function() {
+    $(".splash").fadeOut(1500);
+  },2500);
+  /*
+  *función para mostrar contenido de la página
+  */
+  setTimeout(function() {
+      $(".content").fadeIn(1500);
+  },3500);
+
+  $("#sushi").click(function(){
+    $('.random').css('display', 'none');
+    $('.chinese').css('display', 'none');
+    $('.peruvian').css('display', 'none');
+    $('.sushi').css('display','block')
+  })
+  $("#chinese").click(function(){
+    $('.random').css('display', 'none');
+    $('.peruvian').css('display', 'none');
+    $('.sushi').css('display', 'none');
+    $('.chinese').css('display','block')
+  })
+  $("#peruvian").click(function(){
+    $('.random').css('display', 'none');
+    $('.chinese').css('display', 'none');
+    $('.sushi').css('display', 'none');
+    $('.peruvian').css('display','block')
+  })
 });
