7bd97cd6238f9e8c33f782c4f6bf8682beea15eb
mickequeen <mickequeen@gmail.com>
2017-12-13 11:45:37 -0300

    modals en comida china

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,23 +11,58 @@ $(document).ready(function() {
   setTimeout(function() {
       $(".content").fadeIn(1500);
   },3500);
-
+  /*
+  *funciones para aplicar filtros
+  */
   $("#sushi").click(function(){
     $('.random').css('display', 'none');
     $('.chinese').css('display', 'none');
     $('.peruvian').css('display', 'none');
     $('.sushi').css('display','block')
-  })
+  });
   $("#chinese").click(function(){
     $('.random').css('display', 'none');
     $('.peruvian').css('display', 'none');
     $('.sushi').css('display', 'none');
     $('.chinese').css('display','block')
-  })
+  });
   $("#peruvian").click(function(){
     $('.random').css('display', 'none');
     $('.chinese').css('display', 'none');
     $('.sushi').css('display', 'none');
     $('.peruvian').css('display','block')
+  });
+  /*
+  *funciones para modificar contenido de modal
+  */
+  $('#chineseOne').click(function(){
+    $('#lineModalLabel').text('El Wok');
+    $('#subtitle').text('Comida china al paso');
+    $('#adress').text('Francia #5465');
+    $('#price').text('$2990-$19.990')
+  })
+  $('#chineseTwo').click(function(){
+    $('#lineModalLabel').text('China Town');
+    $('#subtitle').text('Bar & Restaurant');
+    $('#adress').text('Inglaterra #1267');
+    $('#price').text('$5990-$29.990')
+  })
+  $('#chineseThree').click(function(){
+    $('#lineModalLabel').text('Xing Fu');
+    $('#subtitle').text('Comida China para llevar');
+    $('#adress').text('Belisario Prats #1867');
+    $('#price').text('$1990-$15.990')
+  })
+  $('#chineseFour').click(function(){
+    $('#lineModalLabel').text('Fu Lin');
+    $('#subtitle').text('Fusión asiática');
+    $('#adress').text('Lafayette #167');
+    $('#price').text('$4990-$17.990')
+  })
+  $('#chineseFive').click(function(){
+    $('#lineModalLabel').text('Hao Hwa');
+    $('#subtitle').text('Ambiente relajado, buenos precios');
+    $('#adress').text('Santo Domingo #769');
+    $('#price').text('$3990-$16.990')
   })
 });
